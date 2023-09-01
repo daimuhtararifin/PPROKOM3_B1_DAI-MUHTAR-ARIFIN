3df3cb780d2d03b16b9ecb447fc91e0f914d82f2
daimuhtararifin <143492101+daimuhtararifin@users.noreply.github.com>
2023-09-01 16:32:13 +0700

    Add files via upload

---
+++ Tugas 4.py
@@ -0,0 +1,12 @@
+str="Py'thon"
+str
+str2='Py"thon'
+str2
+"\"OK,\" sampai ketemu lagi."
+teks="Python adalah bahasa pemrograman yang powerfull.\Terbukti Pyhtom bisa dijalankan di segala platform OS.\nJadi, satnya kita menggunakan Python sebagai bahasa pemrograman \nsehari-hari. Salam Pyhton Dahsyat!"
+print(teks)
+kata="""Python adalah bahasa pemrograman yang powerfull.
+Terbukti Python bisa dijalankan di segala platform OS.
+Jadi, saatnya kita menggunakan Python sebagai bahasa pemrograman
+sehari-hari. Salam Python Dahsyat!"""
+print(kata)
---
+++ Tugas 9.py
@@ -0,0 +1,5 @@
+print("Dai Muhtar Arifin")
+print("23/519173/SV/23141")
+str='23/519173/SV/23141'
+s=str.replace('/','')
+print(s)
